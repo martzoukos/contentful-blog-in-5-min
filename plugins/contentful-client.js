2a8fdd7e3d5f060af670744bceda07bbe67e0ddf
stefan judis <stefanjudis@gmail.com>
2017-05-12 16:49:30 +0200

    Fix config handling

--- plugins/contentful-client.js
+++ plugins/contentful-client.js
@@ -5,7 +5,7 @@ try {
 } catch (e) {
   if (
     !process.env.CTF_PERSON_ID ||
-    !process.env.CTF_BLOG_POST_ID ||
+    !process.env.CTF_BLOG_POST_TYPE_ID ||
     !process.env.CTF_SPACE_ID ||
     !process.env.CTF_ACCESS_TOKEN
   ) {
@@ -15,7 +15,7 @@ try {
 
   ctfConfig = {
     CTF_PERSON_ID: process.env.CTF_PERSON_ID,
-    CTF_BLOG_POST_ID: process.env.CTF_BLOG_POST_ID,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_ACCESS_TOKEN: process.env.CTF_ACCESS_TOKEN
   }
